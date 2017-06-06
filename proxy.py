f19cc812c26ca26079404a460d93058ce42b0d0f
Your Name <you@example.com>
2017-06-06 15:28:46 -0300

    proxyyyy

--- proxy.py
+++ proxy.py
@@ -14,6 +14,7 @@ print("oi")
 
 class ProxyServer(object):
     """docstring for ."""
+    proibido = 'jogo'
 
     def __init__(self, host, port):
         self.host = host
@@ -31,16 +32,53 @@ class ProxyServer(object):
             threading.Thread(target=self.client, args=(sock_client, addr_client)).start()
 
     def client(self, sock_client, addr):
-        size = 1024
-        while True:
-            data = sock_client.recv(size)
-            if data:
-                # Set the response to echo back the recieved data
-                response = data
-                sock_client.send(response)
-            else:
-                sock_client.close()
-                break
+        size = 10000
+        data = sock_client.recv(size)
+        if not data: return 'sem dados'
+        
+        print("\n---- header recebido ---------------------------\n")
+        datastr = data.decode()
+        print(datastr)
+        header = datastr.split('\n')
+        command, site, protocol = header[0].split(' ')
+        host = header[1].split(' ')[1]
+        command.strip()
+        site.strip()
+        protocol.strip()
+        host.strip()
+        print('command: {}'.format(command))
+        print('site: {}'.format(site))
+        print('protocol: {}'.format(protocol))
+        print('host: {}'.format(host))
+ 
+        if self.proibido in datastr:
+            sock_client.send(b'HTTP/1.0 200 OK\n')
+            sock_client.send(b'Content-Type: text/html\n')
+            sock_client.send(b'\n') # header and body should be separated by additional newline
+            sock_client.send('<html><body> <h1>Hello World</h1> Nao pode!! </body></html>'.encode())
+        else:
+            # Set the response to echo back the recieved data
+            #sock_client.send(b'HTTP/1.0 200 OK\n')
+            #sock_client.send(b'Content-Type: text/html\n')
+            #sock_client.send(b'\n') # header and body should be separated by additional newline
+            #sock_client.send('<html><body> <h1>Hello World</h1> Esse pode!! </body></html>'.encode())
+            resposta = self.pega_site(command, site, host)
+            sock_client.send(resposta)
+        sock_client.close()
+
+    def pega_site(self, command, site, host):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.connect((host, 80))
+        print('conectando com {}:{}'.format(host, 80))
+        sock.send('''{} {} HTTP/1.1\n
+                Host: {}\n
+                User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0\n
+                Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\n
+                Accept-Language: en-US,en;q=0.5\n
+                Accept-Encoding: gzip, deflate\n
+                Connection: keep-alive\n'''.format(command, site, host).encode())
+        return sock.recv(10000)
 
 
 if __name__ == '__main__':
