87770f79f2b97954680b1d2706ee87ed21f2a08e
tomasabril <tomas.abril@gmail.com>
2017-06-07 00:19:50 -0300

    era pra estar bom mas n está tanto ainda

--- proxy.py
+++ proxy.py
@@ -9,12 +9,15 @@
 import socket
 import threading
 import sys
+import time
 
 print("oi")
 
 class ProxyServer(object):
     """docstring for ."""
+
     proibido = 'jogo'
+    size = 1024000
 
     def __init__(self, host, port):
         self.host = host
@@ -28,57 +31,57 @@ class ProxyServer(object):
         self.sock.listen(5)
         while True:
             sock_client, addr_client = self.sock.accept()
-            sock_client.settimeout(60)
+            # sock_client.settimeout(60)
             threading.Thread(target=self.client, args=(sock_client, addr_client)).start()
 
     def client(self, sock_client, addr):
-        size = 10000
-        data = sock_client.recv(size)
+        data = sock_client.recv(self.size)
+        time.sleep(.1)
         if not data: return 'sem dados'
-        
-        print("\n---- header recebido ---------------------------\n")
+
+        print("\n---- header recebido ---------------------------")
         datastr = data.decode()
-        print(datastr)
-        header = datastr.split('\n')
+
+        header = datastr.split('\r\n')
         command, site, protocol = header[0].split(' ')
         host = header[1].split(' ')[1]
-        command.strip()
-        site.strip()
-        protocol.strip()
-        host.strip()
-        print('command: {}'.format(command))
-        print('site: {}'.format(site))
-        print('protocol: {}'.format(protocol))
-        print('host: {}'.format(host))
- 
-        if self.proibido in datastr:
-            sock_client.send(b'HTTP/1.0 200 OK\n')
-            sock_client.send(b'Content-Type: text/html\n')
-            sock_client.send(b'\n') # header and body should be separated by additional newline
-            sock_client.send('<html><body> <h1>Hello World</h1> Nao pode!! </body></html>'.encode())
-        else:
-            # Set the response to echo back the recieved data
-            #sock_client.send(b'HTTP/1.0 200 OK\n')
-            #sock_client.send(b'Content-Type: text/html\n')
-            #sock_client.send(b'\n') # header and body should be separated by additional newline
-            #sock_client.send('<html><body> <h1>Hello World</h1> Esse pode!! </body></html>'.encode())
-            resposta = self.pega_site(command, site, host)
-            sock_client.send(resposta)
-        sock_client.close()
 
-    def pega_site(self, command, site, host):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.connect((host, 80))
-        print('conectando com {}:{}'.format(host, 80))
-        sock.send('''{} {} HTTP/1.1\n
-                Host: {}\n
-                User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0\n
-                Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\n
-                Accept-Language: en-US,en;q=0.5\n
-                Accept-Encoding: gzip, deflate\n
-                Connection: keep-alive\n'''.format(command, site, host).encode())
-        return sock.recv(10000)
+        if command == 'GET':
+            print(datastr)
+            print('command:  -{}-'.format(command))
+            print('site:     -{}-'.format(site))
+            print('protocol: -{}-'.format(protocol))
+            print('host:     -{}-'.format(host))
+
+        if self.proibido in site:
+            sock_client.send(b'HTTP/1.0 200 OK\r\n')
+            sock_client.send(b'Content-Type: text/html\r\n')
+            sock_client.send(b'\r\n') # header and body should be separated by additional newline
+            sock_client.send('<html><body> <h1>Acesso nao autorizado!</h1> Nao pode!! </body></html>'.encode())
+        else:
+            print('\nconectando com {}:{}'.format(host, 80))
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            # sock.settimeout(30)
+            if command == 'GET':
+                sock.connect((host, 80))
+            elif command == 'CONNECT':
+                conhost, conport = datastr.split('Host: ')[1].split(':')
+                print('HOST: {}'.format(conhost))
+                print('PORT: {}'.format(conport))
+                sock.connect((conhost, int(conport)))
+            else:
+                print('================= erro ================ \ncomando não reconhecido')
+            print('enviando dados...')
+            sock.send(data)
+            print('esperando resposta...')
+            resposta = sock.recv(self.size)
+            print('mandando resposta para browser...')
+            sock_client.send(resposta)
+            time.sleep(10)
+            sock.close()
+        print('fechando conexão...')
+        sock_client.close()
 
 
 if __name__ == '__main__':
